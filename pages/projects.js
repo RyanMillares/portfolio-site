d25b5238d23618c9c8382a0640682a528b6d57e5
RyanMillares <rmillares@chapman.edu>
2023-08-24 12:38:23 -0700

    added top margin to projects filter on desktop view

--- pages/projects.js
+++ pages/projects.js
@@ -196,7 +196,7 @@ export default function Projects({ pageProps, prevRoute, currentRoute }) {
 
                             className="min-w-[25vw] max-w-[25vw]"
                         >
-                            <div className="ml-4 fixed">
+                            <div className="ml-4 fixed top-14">
                                 <div className="bg-gray-800 rounded-tr-lg rounded-br-lg sm:rounded-lg min-w-[25vw] max-w-[25vw] p-4 mr-5">
                                     <h1 className="text-white">Filter by Setting</h1>
                                     <br />
